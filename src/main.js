88a67575552c934cc227fc9b9d2dc6f79048941c
illuminatordeath <57045091+illuminatordeath@users.noreply.github.com>
2024-05-19 17:44:32 +0300

    Update main.js

--- src/main.js
+++ src/main.js
@@ -1,8 +1,16 @@
 const cron = require('node-cron');
 const axios = require('axios');
 const config = require('config');
+const { Client } = require('@notionhq/client');
+const notion = new Client({ auth: config.get('NOTION_ATI_KEY')});
+const YOUTUBE_API_KEY = config.get('YOUTUBE_API_KEY');
+const CHANNEL_ID = config.get('CHANNEL_ID');
+const NOTION_DATABASE_ID = config.get('NOTION_DATABASE_ID');
+const VIEWS_BLOCK_ID = config.get('VIEWS_BLOCK_ID')
+const SUBSCRIBERS_BLOCK_ID = config.get('SUBSCRIBERS_BLOCK_ID')
+const VIDEOS_BLOCK_ID = config.get('VIDEOS_BLOCK_ID')
 
-cron.schedule('0 */12 * * *', () => {
+cron.schedule('0 */6 * * *', () => {
 async function fetchYouTubeData(apiKey, channelId) {
     const url = `https://www.googleapis.com/youtube/v3/channels?part=statistics&id=${channelId}&key=${apiKey}`;
 
@@ -19,37 +27,60 @@ async function fetchYouTubeData(apiKey, channelId) {
         throw error;
     }
 }
-const { Client } = require('@notionhq/client');
-
-const notion = new Client({ auth: config.get('NOTION_ATI_KEY')});
 
 async function updateNotionDatabase(databaseId, data) {
     try {
-        const pageId = config.get('PAGE_ID');
-        const response = await notion.pages.update({
-            page_id: pageId,
-            properties: {
-                'Views': {
-                  "number": parseInt(data.viewCount)
-                },
-                'Subscribers': {
-                  "number": parseInt(data.subscriberCount)
-                },
-                'Videos': {
-                  "number": parseInt(data.videoCount)
-                }
-              },
+        const responseViews = await notion.blocks.update({
+            "block_id": VIEWS_BLOCK_ID,
+            "heading_2": {
+                "rich_text": [
+                    {
+                        "text": {
+                            "content": data.viewCount
+                        },
+                        "annotations": {
+                            "color": "red"
+                        }
+                    }
+                ]
+            }
         });
-        console.log('Notion database updated successfully:', response);
+        const responseSubscribers = await notion.blocks.update({
+            "block_id": SUBSCRIBERS_BLOCK_ID,
+            "heading_2": {
+                "rich_text": [
+                    {
+                        "text": {
+                            "content": data.subscriberCount
+                        },
+                        "annotations": {
+                            "color": "green"
+                        }
+                    }
+                ]
+            }
+        });
+        const responseVideos = await notion.blocks.update({
+            "block_id": VIDEOS_BLOCK_ID,
+            "heading_2": {
+                "rich_text": [
+                    {
+                        "text": {
+                            "content": data.videoCount
+                        },
+                        "annotations": {
+                            "color": "yellow"
+                        }
+                    }
+                ]
+            }
+        });
+        console.log('Notion database updated successfully:');
     } catch (error) {
         console.error('Failed to update Notion database:', error);
         throw error;
     }
 }
-
-const YOUTUBE_API_KEY = config.get('YOUTUBE_API_KEY');
-const CHANNEL_ID = config.get('CHANNEL_ID');
-const NOTION_DATABASE_ID = config.get('NOTION_DATABASE_ID');
 
 async function syncYouTubeToNotion() {
     try {
